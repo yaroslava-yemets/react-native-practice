8e826804328e506960a6f5119165619a50128e91
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-12-04 19:55:54 +0200

    added todo list

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React from 'react';
-import { extendTheme, NativeBaseProvider, Box, Text } from 'native-base';
+import { NativeBaseProvider, Box, Text } from 'native-base';
 
-// import AddButton from './components/AddButton';
-// import TaskInput from './components/TaskInput';
-import AddTaskBox from './components/AddTaskBox';
-import TodoList from './components/TodoList';
+import ToDoList from './components/ToDoList';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <TodoList />
-      <AddTaskBox />
-      <Box flex={1} bg="#941c1c" alignItems="center" justifyContent="center">
-        <Text color="#469e9e">Open up App.js to start working on your app!</Text>
-      </Box>
+      <ToDoList />
     </NativeBaseProvider>
   );
 }
 
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>This is my firs expo app</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,46 +1,18 @@
-import React from "react"
-import {
-  Input,
-  IconButton,
-  Checkbox,
-  Text,
-  Box,
-  VStack,
-  HStack,
-  Heading,
-  Icon,
-  Center,
-  NativeBaseProvider,
-} from "native-base"
-import { Feather, Entypo } from "@expo/vector-icons"
-export const Example = () => {
-  const instState = [
-    {
-      title: "Code",
-      isCompleted: true,
-    },
-    {
-      title: "Meeting with team at 9",
-      isCompleted: false,
-    },
-    {
-      title: "Check Emails",
-      isCompleted: false,
-    },
-    {
-      title: "Write an article",
-      isCompleted: false,
-    },
-  ]
-  const [list, setList] = React.useState(instState)
-  const [inputValue, setInputValue] = React.useState("")
+import React from 'react';
+import { useState } from 'react';
+import { Box, Heading, Center, Input, IconButton, Icon, HStack, VStack, Checkbox, Text } from 'native-base';
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons"
 
-  const addItem = (title) => {
+export default function ToDoList() {
+  const [inputValue, setInputValue] = useState("")
+  const [list, setList] = useState([])
+
+  const addTask = (task) => {
     setList([
-      ...list,
+      ...list, 
       {
-        title: title,
-        isCompleted: false,
+        task,
+        isCompleted: false
       },
     ])
   }
@@ -57,82 +29,120 @@ export const Example = () => {
     setList(temp)
   }
 
-  return (
-    <Box>
-      <Heading mb="5">Wednesday</Heading>
-      <VStack space={4}>
-        <HStack space={2}>
-          <Input
-            flex={1}
-            onChangeText={(v) => setInputValue(v)}
-            value={inputValue}
-            placeholder="Add Task"
-          />
-          <IconButton
-            borderRadius="sm"
-            variant="solid"
-            icon={
-              <Icon as={Feather} name="plus" size="sm" color="warmGray.50" />
-            }
-            onPress={() => {
-              addItem(inputValue)
-              setInputValue("")
-            }}
-          />
-        </HStack>
-        <VStack space={2}>
-          {list.map((item, itemI) => (
-            <HStack
-              w="100%"
-              justifyContent="space-between"
-              alignItems="center"
-              key={item.title + itemI.toString()}
-            >
+    return (
+      <Center p={10} flex={1}>
+        <Box 
+          w="100%" 
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading mb="5" color="orange.500">To do List</Heading>
+          <VStack space={3} width="100%">
+            <HStack space={3} justifyContent="space-between">
+              <Input 
+                flex={1}
+                fontSize={15}
+                color="orange.500"
+                borderColor="orange.500"
+                _focus={{
+                  borderColor: "orange.600:alpha.20",
+                }}
+                onChangeText={(v) => setInputValue(v)}
+                value={inputValue}
+                placeholder="Add Task"
+              />
+              <IconButton
+                onPress={() => {
+                  addTask(inputValue)
+                  setInputValue("")
+                }}
+                icon={<Icon as={Ionicons} name="ios-add-circle" />}
+                borderRadius="full"
+                _icon={{
+                  color: "orange.500",
+                  size: "md",
+                }}
+                _hover={{
+                  bg: "orange.600:alpha.20",
+                }}
+                _pressed={{
+                  bg: "orange.600:alpha.20",
+                  _icon: {
+                    name: "ios-add-circle",
+                  },
+                  _ios: {
+                    _icon: {
+                      size: "3xl",
+                    },
+                  },
+                }}
+                _ios={{
+                  _icon: {
+                    size: "3xl",
+                  },
+                }}
+              />
+            </HStack>
+
+            {list.map((item, itemI) => (
+              <HStack 
+                justifyContent="space-between"
+                alignItems="center"
+                space={5}
+                key={item.task + itemI.toString()}
+              >
               <Checkbox
+                colorScheme="orange"
+                borderColor="orange.500:alpha.20"
                 isChecked={item.isCompleted}
                 onChange={() => handleStatusChange(itemI)}
-                value={item.title}
+                value={item.task}
+                icon={<Icon as={<Ionicons name="md-checkmark-done" />} />}
+                _hover={{
+                  borderColor: "orange.500"
+                }}
+                _focus={{
+                  borderColor: "orange.500"
+                }}
               >
                 <Text
-                  mx="2"
+                  mx={3}
+                  fontStyle="italic"
+                  fontSize={15}
                   strikeThrough={item.isCompleted}
                   _light={{
-                    color: item.isCompleted ? "gray.400" : "coolGray.800",
-                  }}
-                  _dark={{
-                    color: item.isCompleted ? "gray.400" : "coolGray.50",
+                    color: item.isCompleted ? "orange.600:alpha.30" : "orange.500",
                   }}
                 >
-                  {item.title}
+                  {item.task}
                 </Text>
               </Checkbox>
               <IconButton
                 size="sm"
-                colorScheme="trueGray"
+                colorScheme="orange"
+                color="orange.500"
                 icon={
                   <Icon
-                    as={Entypo}
-                    name="minus"
-                    size="xs"
-                    color="trueGray.400"
+                    as={MaterialCommunityIcons}
+                    name="delete"
+                    size="lg"
+                    color="orange.500"
+                    _hover={{
+                      bg: "orange.600:alpha.20"
+                    }}
+                    _focus={{
+                      bg: "orange.600:alpha.20",
+                    }}
                   />
+                  
                 }
                 onPress={() => handleDelete(itemI)}
               />
             </HStack>
           ))}
-        </VStack>
-      </VStack>
-    </Box>
-  )
-}
 
-export default () => {
-  return (
-    <NativeBaseProvider>
-      <Center flex={1} px="3">
-        <Example />
+          </VStack>
+        </Box>
       </Center>
-    </NativeBaseProvider>
-  )
+    );
 }
